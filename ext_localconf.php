180bb5f716d03e1d5d696240af2b6f0114efb488
Peter Kraume <peter.kraume@gmx.de>
2015-12-02 08:52:38 +0100

    remove usage of 'use' to prevent merge conflicts

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,14 +1,13 @@
 <?php
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-if (ExtensionManagementUtility::isLoaded('fluid_styled_content')) {
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('fluid_styled_content')) {
 
 	// Add our new content element to the "New Content Element Wizard" using Page TSconfig
-	ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TSconfig/Page/demo_fsc.txt">');
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TSconfig/Page/demo_fsc.txt">');
 
 	// Register for hook to show preview of tt_content element of CType="demo_fsc" in page module
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['demo_fsc'] = \KWS\DemoFsc\Hooks\PageLayoutView\DemoFscPreviewRenderer::class;
